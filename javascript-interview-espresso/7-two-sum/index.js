545e7659a69e53fefe5c2e4468f4b905efa9c845
Sunil Bagde <bagdesunil786@gmail.com>
2020-08-17 15:39:16 +0530

    wip

---
+++ javascript-interview-espresso/7-two-sum/index.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+function twoSum(nums, target) {
+    let map = {};
+    for (var i = 0; i < nums.length; i++) {
+        let num = nums[i];
+
+        let want = target - num;
+        if (want in map) {
+            let leftIndex = map[want];
+            return [leftIndex, i];
+        } else {
+            map[num] = i;
+        }
+    }
+}
+
+let result = twoSum([2, 7, 11, 15], 9);
+console.log("result", result);
